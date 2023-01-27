aa4184f3f1d2155d9031fe879afbfcb54a01e558
Parmanand Parveen <115132837+Parmanand-Parveen@users.noreply.github.com>
2023-01-27 20:51:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    
+    extend: { animation: {
+      'spin-slow': 'spin 55s linear infinite',
+    },
+    colors: {
+      voilate2:"#f1e4ff",
+      help:"#6a78cd",
+    },
+  
+
+     
+    },
+  },
+  plugins: [],
+}
